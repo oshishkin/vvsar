4eb50aca83108599b415960cb3aa3d22e9b6f33a
osh <oshishkin@gmail.com>
2018-04-20 21:57:56 +0200

    Nobelstraße data request and parse

--- index.js
+++ index.js
@@ -6,19 +6,25 @@ var body = '<?xml version="1.0" encoding="UTF-8"?>'+
 '<RequestPayload>'+
 '<StopEventRequest>'+
 '<Location><LocationRef>'+
-'<GeoPosition>'+
-'<Longitude>9.15949</Longitude>'+
-'<Latitude>48.77080</Latitude>'+
-'</GeoPosition>'+
-'<LocationName>'+
-'<Text>Rotebühlstraﬂe</Text>'+
-'</LocationName>'+
+//'<StopPointRef>de:08111:6008:1:1</StopPointRef>'+  //Uni
+'<StopPointRef>de:08111:6018:0:4</StopPointRef>'+    // Nobelstr
+
+//'<GeoPosition>'+
+//'<Longitude>9.15949</Longitude>'+
+//'<Latitude>48.77080</Latitude>'+
+
+//'<Longitude>9.10583696296453</Longitude>'+
+//'<Latitude>48.7450905386822</Latitude>'+
+//'</GeoPosition>'+
+//'<LocationName>'+
+//'<Text>Wangener-/Landhausstraße</Text>'+
+//'</LocationName>'+
 '</LocationRef></Location>'+
 '<Params>'+
-'<NumberOfResults>20</NumberOfResults>'+
+'<NumberOfResults>4</NumberOfResults>'+
 '<StopEventType>departure</StopEventType>'+
-'<PtModeFilter>Bus</PtModeFilter>'+
-'<BusSubmode>localBus</BusSudmode>'+
+//'<PtModeFilter>Bus</PtModeFilter>'+
+//'<BusSubmode>localBus</BusSudmode>'+
 '</Params>'+
 '</StopEventRequest>'+
 '</RequestPayload>'+
@@ -54,7 +60,27 @@ var req = http.request( postRequest, function( res )    {
    res.on( "end", function(  ) { 
 
         parser.parseString(buffer, function (err, result) {
-            console.log(util.inspect(result.Trias.ServiceDelivery,false, null));
+
+            let resultArr = result.Trias.ServiceDelivery[0].DeliveryPayload[0].StopEventResponse[0].StopEventResult;
+
+            for (i=0; i<4; i++)
+            {
+                
+
+                let stopEvent = resultArr[i].StopEvent[0];
+                let stopData = stopEvent.ThisCall[0].CallAtStop;
+                let serviceData = stopEvent.Service[0];
+
+                //console.log(util.inspect(serviceData,false, null));
+
+                console.log(">>>>>");
+                console.log("Stop Name: "+stopData[0].StopPointName[0].Text);
+                console.log("Departure time: "+stopData[0].ServiceDeparture[0].TimetabledTime);
+                console.log("Linie Name: "+serviceData.Mode[0].Name[0].Text+" "+serviceData.PublishedLineName[0].Text);
+                console.log("Linie Description: "+serviceData.RouteDescription[0].Text);
+
+            }
+
             console.log('Done');
         });   
 
