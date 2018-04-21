a5533c1339e04f9f066cd8185e23259c7428526e
Sergey Zhusman <zhusmansan@gmail.com>
2018-04-21 13:47:56 +0300

    Fix api signature

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-//const httpPort = 80;
-const httpPort = 8080;
+// import { resolve } from 'url';
+
+const httpPort = 80;
+// const httpPort = 8080;
 let http = require('http');
+var request = require('sync-request');
 let json2xml = require('json2xml'); 
 let xml2js = require('xml2js');
 var parser = new xml2js.Parser();
 const util = require('util')
 var express = require('express');
 const app = express();
-
+const GTFS_STOPS = [{stop_id:"de:08111:6018:0:4", stop_name:"Nobelstraße", stop_lat:48.740356600365, stop_lon:9.10019824732889}];
 
 app.use('/', express.static('public'));
 app.route('/').get(function (req, res) {
@@ -58,59 +61,84 @@ function requestStopInfo(reqData, cbFunction){
             'Content-Length': Buffer.byteLength(body)
         }
     };
-    var req = http.request( postRequest, function( res )    {
+    var promise1 = new Promise(function(resolve, reject) {
+        var req = http.request( postRequest, function( res )    {
 
-    console.log( res.statusCode );
-        var buffer = "";
-        res.on( "data", function( data ) { buffer = buffer + data; } );
-        res.on( "end", function(  ) { 
-     
-             parser.parseString(buffer, function (err, result) {
-     
-                let resultArr = result.Trias.ServiceDelivery[0].DeliveryPayload[0].StopEventResponse[0].StopEventResult;
-                var mappedResult = resultArr.map(function(current){
-                    return {
-                        // nextStopName: current.StopEvent[0].ThisCall[0].CallAtStop[0].StopPointName[0].Text[0],
-                        departureTime: current.StopEvent[0].ThisCall[0].CallAtStop[0].ServiceDeparture[0].TimetabledTime[0],
-                        lineType:current.StopEvent[0].Service[0].Mode[0].Name[0].Text[0],
-                        lineName:current.StopEvent[0].Service[0].PublishedLineName[0].Text[0],
-                        routeDescription:current.StopEvent[0].Service[0].RouteDescription[0].Text[0],
-                        startName:current.StopEvent[0].Service[0].OriginText[0].Text[0],
-                        stopName:current.StopEvent[0].Service[0].DestinationText[0].Text[0]
-                    };
-                });
-                console.log("Stop event=="+util.inspect(result,false, null));
-                cbFunction({stationName:resultArr[0].StopEvent[0].ThisCall[0].CallAtStop[0].StopPointName[0].Text[0], timetable:mappedResult});
-             });   
-     
-     
-             //console.log( buffer ); 
-         } );
-     
-     });
-     
-     req.on('error', function(e) {
-         console.log('problem with request: ' + e.message);
-     });
-     
-     req.write( body );
+            console.log( res.statusCode );
+                var buffer = "";
+                res.on( "data", function( data ) { buffer = buffer + data; } );
+                res.on( "end", function(  ) { 
+             
+                     parser.parseString(buffer, function (err, result) {
+             
+                        let resultArr = result.Trias.ServiceDelivery[0].DeliveryPayload[0].StopEventResponse[0].StopEventResult;
+                        var mappedResult = resultArr.map(function(current){
+                            return {
+                                // nextStopName: current.StopEvent[0].ThisCall[0].CallAtStop[0].StopPointName[0].Text[0],
+                                departureTime: current.StopEvent[0].ThisCall[0].CallAtStop[0].ServiceDeparture[0].TimetabledTime[0],
+                                lineType:current.StopEvent[0].Service[0].Mode[0].Name[0].Text[0],
+                                lineName:current.StopEvent[0].Service[0].PublishedLineName[0].Text[0],
+                                routeDescription:current.StopEvent[0].Service[0].RouteDescription[0].Text[0],
+                                startName:current.StopEvent[0].Service[0].OriginText[0].Text[0],
+                                stopName:current.StopEvent[0].Service[0].DestinationText[0].Text[0]
+                            };
+                        });
+                        console.log("Stop event=="+util.inspect(mappedResult,false, null));
+                        resolve({stationName:resultArr[0].StopEvent[0].ThisCall[0].CallAtStop[0].StopPointName[0].Text[0], timetable:mappedResult});
+                     });   
+             
+             
+                     //console.log( buffer ); 
+                 } );
+             
+             });
+             
+             req.on('error', function(e) {
+                 console.log('problem with request: ' + e.message);
+             });
+             
+             req.write( body );
+             req.end();
+                
+    });
+    return promise1;  
 
 }
 
+function requestWeatherInfo(latlng)
+{
+    // var currentWeather;
+    // currentWeather = get('http://api.openweathermap.org/data/2.5/weather/?appid=7b6e364a6ea88bdfd9ab285f380cb898&lat=' + latlng.lat + '&lon='+latlng.lng);
+    var currentWeather = JSON.parse(request('GET', 'http://api.openweathermap.org/data/2.5/weather/?appid=7b6e364a6ea88bdfd9ab285f380cb898&lat=' + latlng.lat + '&lon='+latlng.lng).body.toString('utf-8'));
+    console.log(currentWeather);
+    var weatherAtSunrise = JSON.parse(request('GET', 'http://api.openweathermap.org/data/2.5/weather/?appid=7b6e364a6ea88bdfd9ab285f380cb898&lat=' + latlng.lat + '&lon='+latlng.lng).body.toString('utf-8'));
+    console.log(weatherAtSunrise);
+}
+
+
+// requestWeatherInfo({lat:48.740356600365, lng:9.10019824732889});
+
 app.get("/api/stop", (req, res) => {
-    requestStopInfo({LocationRef:{
-        StopPointRef:"de:08111:6018:0:4"
-    }},function(value){
-        value.timetable = value.timetable.slice(0,4);
-        //hardcode FIXME
-        value["weather"]={now: {temp: 21}, 
-            sunrise: {time: '0:36', temp:16 }, 
-            sunset: {time: '18:36', temp:13 }};
+    requestStopInfo(
+        {
+            LocationRef: {
+                StopPointRef: GTFS_STOPS[0].stop_id
+            }
+        }
+    ).then(value =>{
+
+        value.timetable = value.timetable.slice(0, 4);
+        value["weather"] = {
+            now: { temp: 21 },
+            sunrise: { time: '0:36', temp: 16 },
+            sunset: { time: '18:36', temp: 13 }
+        };
         console.log(value);
         res.send(value);
-    });     
-    
+
+    });
 });
+
 
 app.use(express.static('public'));
 
