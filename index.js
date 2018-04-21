82e0a580cb2b140c9700d86f4ac704e62e7b96b5
Sergey Zhusman <zhusmansan@gmail.com>
2018-04-21 12:02:10 +0300

    Minor api fixes

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const httpPort = 80;
+// const httpPort = 8085;
 let http = require('http');
 let json2xml = require('json2xml'); 
 let xml2js = require('xml2js');
@@ -68,7 +70,7 @@ function requestTriasApi(reqData, cbFunction){
                 let resultArr = result.Trias.ServiceDelivery[0].DeliveryPayload[0].StopEventResponse[0].StopEventResult;
                 var mappedResult = resultArr.map(function(current){
                     return {
-                        nextStopName: current.StopEvent[0].ThisCall[0].CallAtStop[0].StopPointName[0].Text[0],
+                        // nextStopName: current.StopEvent[0].ThisCall[0].CallAtStop[0].StopPointName[0].Text[0],
                         departureTime: current.StopEvent[0].ThisCall[0].CallAtStop[0].ServiceDeparture[0].TimetabledTime[0],
                         lineType:current.StopEvent[0].Service[0].Mode[0].Name[0].Text[0],
                         lineName:current.StopEvent[0].Service[0].PublishedLineName[0].Text[0],
@@ -77,8 +79,8 @@ function requestTriasApi(reqData, cbFunction){
                         stopName:current.StopEvent[0].Service[0].DestinationText[0].Text[0]
                     };
                 });
-                console.log("Stop event=="+util.inspect(resultArr,false, null));
-                cbFunction({timetable:mappedResult});
+                console.log("Stop event=="+util.inspect(result,false, null));
+                cbFunction({stationName:resultArr[0].StopEvent[0].ThisCall[0].CallAtStop[0].StopPointName[0].Text[0], timetable:mappedResult});
              });   
      
      
@@ -99,28 +101,19 @@ app.get("/api", (req, res) => {
     requestTriasApi({LocationRef:{
         StopPointRef:"de:08111:6018:0:4"
     }},function(value){
+        value.timetable = value.timetable.slice(0,4);
+        //hardcode FIXME
+        value["weather"]={now: {temp: 21}, 
+            sunrise: {time: '0:36', temp:16 }, 
+            sunset: {time: '18:36', temp:13 }};
         console.log(value);
-        res.send(value)
+        res.send(value);
     });     
     
 });
 
 app.use(express.static('public'));
 
-app.get("/renderimage.svg", (req, res) => {
-    console.log(req.params.text);
-    res.setHeader('content-type', 'image/svg+xml')
-    res.send(`<svg xmlns="http://www.w3.org/2000/svg" width="256" height="256">
-            <foreignObject width="100%" height="100%">
-                <div style="font-size: 40px;" xmlns="http://www.w3.org/1999/xhtml">
-                    <em>I</em>
-                    like
-                    <span style="color: white; text-shadow: 0 0 2px black;" id="like">cheese</span>
-                </div> 
-            </foreignObject>
-        </svg>`)
-})
-
-app.listen(80, () => {
-    console.info("App is running at port 80"); 
+app.listen(httpPort, () => {
+    console.info("App is running at port "+httpPort); 
 })
